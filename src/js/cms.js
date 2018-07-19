979930578faad797563afe0e7a289b0f95405186
KEENPUB\sopon.g <sopon.g@quo-global.com>
2018-07-19 17:44:22 +0700

    gg

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,11 +1,15 @@
-$(document).on('click','.new',function(event)
-{
-    event.preventDefault();
-$('.cms').css("display","block");
-});
+// $(document).on('click','#correct',function(event)
+// {
+//     event.preventDefault();
+// $('.cms').css("display","block");
+// $('.login').css("display","none");
+// });
 
 // $(document).on('click','#create_item',function(my){
 //     event.preventDefault();
 //  $('#form_new_item').before($('#form_new_item').html());
 
 // });
+function refresh_page_cms(){
+    window.location="/cms.php";
+}
